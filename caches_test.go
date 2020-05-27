9803385d466cfc612d46df4b28af03e6b5273a6a
teivah <t.harsanyi@thebeat.co>
2020-05-27 19:39:08 +0200

    Bench tests

---
+++ caches_test.go
@@ -0,0 +1,147 @@
+package caches
+
+import (
+	"sync"
+	"testing"
+)
+
+const (
+	M                = 1_000_000
+	CacheLinePadSize = 64
+)
+
+type SimpleStruct struct {
+	n int
+}
+
+type PaddedStruct struct {
+	n int
+	_ CacheLinePad
+}
+
+type CacheLinePad struct {
+	_ [CacheLinePadSize]byte
+}
+
+func BenchmarkStructureFalseSharing(b *testing.B) {
+	structA := SimpleStruct{}
+	structB := SimpleStruct{}
+	wg := sync.WaitGroup{}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(2)
+		go func() {
+			for j := 0; j < M; j++ {
+				structA.n += j
+			}
+			wg.Done()
+		}()
+		go func() {
+			for j := 0; j < M; j++ {
+				structB.n += j
+			}
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+}
+
+func BenchmarkStructurePadding(b *testing.B) {
+	structA := PaddedStruct{}
+	structB := SimpleStruct{}
+	wg := sync.WaitGroup{}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		wg.Add(2)
+		go func() {
+			for j := 0; j < M; j++ {
+				structA.n += j
+			}
+			wg.Done()
+		}()
+		go func() {
+			for j := 0; j < M; j++ {
+				structB.n += j
+			}
+			wg.Done()
+		}()
+		wg.Wait()
+	}
+}
+
+const matrixLength = 6400
+
+func BenchmarkMatrixSimpleAddition(b *testing.B) {
+	matrixA := createMatrix(matrixLength)
+	matrixB := createMatrix(matrixLength)
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < matrixLength; i++ {
+			for j := 0; j < matrixLength; j++ {
+				matrixA[i][j] = matrixA[i][j] + matrixB[i][j]
+			}
+		}
+	}
+}
+
+func BenchmarkMatrixReversedAddition(b *testing.B) {
+	matrixA := createMatrix(matrixLength)
+	matrixB := createMatrix(matrixLength)
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < matrixLength; i++ {
+			for j := 0; j < matrixLength; j++ {
+				matrixA[i][j] = matrixA[i][j] + matrixB[j][i]
+			}
+		}
+	}
+}
+
+func BenchmarkMatrixReversedAdditionPerBlock(b *testing.B) {
+	matrixA := createMatrix(matrixLength)
+	matrixB := createMatrix(matrixLength)
+	blockSize := 8
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < matrixLength; i += blockSize {
+			for j := 0; j < matrixLength; j += blockSize {
+				for ii := i; ii < i+blockSize; ii++ {
+					for jj := j; jj < j+blockSize; jj++ {
+						matrixA[ii][jj] = matrixA[ii][jj] + matrixB[jj][ii]
+					}
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkMatrixReversedAdditionPerBlockB(b *testing.B) {
+	matrixA := createMatrix(matrixLength)
+	matrixB := createMatrix(matrixLength)
+	blockSize := 8
+
+	for n := 0; n < b.N; n++ {
+		for i := 0; i < matrixLength; i += blockSize {
+			for j := 0; j < matrixLength; j += blockSize {
+				for ii := i; ii < i+blockSize; ii++ {
+					for jj := j; jj < j+blockSize; jj++ {
+						matrixA[ii][jj] = matrixA[ii][jj] + matrixB[jj][ii]
+					}
+				}
+			}
+		}
+	}
+}
+
+func createMatrix(size int) [][]int64 {
+	matrix := make([][]int64, size)
+	for i := 0; i < size; i++ {
+		matrix[i] = make([]int64, size)
+		for j := 0; j < size; j++ {
+			matrix[i][j] = 2
+		}
+	}
+	return matrix
+}
